60d6157be03576ac1dce1589c0659af401bb21ea
Shahid Khan <shahidseran786@gmail.com>
2024-05-02 23:36:29 +0530

    TMDB_API_SETUP

--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -2,3 +2,11 @@ export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const USER_AVTAR =
   "https://i.pinimg.com/564x/5b/50/e7/5b50e75d07c726d36f397f6359098f58.jpg";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: 
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTdlY2RlZDVkOGU1YTBjMzhhMjU4YzRiZTY5YjJiNCIsInN1YiI6IjY2MzNjZmE3NmU5MzhhMDEyNzg5MDdiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H_7j9FjmIsnEeaARI9jJl0UwCOpxh2vrF0FIUArtX-E'
+  },
+};
